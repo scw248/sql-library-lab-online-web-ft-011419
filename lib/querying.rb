4bd2f5457fa5aaae2bc28af947472ebc2b01eb6c
Stephen Wright <scw248@gmail.com>
2019-02-13 17:51:20 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -20,5 +20,5 @@ def select_series_title_with_most_human_characters
 end
 
 def select_character_names_and_number_of_books_they_are_in
-  "SELECT characters.name, COUNT(books.title) AS value_occurrence FROM characters JOIN character_books ON characters.id = character_books.character_id JOIN books ON book.id = character_books.book_id GROUP BY characters.name ORDER BY value_occurrence"
+  "SELECT characters.name, COUNT(books.title) AS value_occurrence FROM characters JOIN character_books ON characters.id = character_books.character_id JOIN books ON books.id = character_books.book_id GROUP BY characters.name ORDER BY value_occurrence"
 end
